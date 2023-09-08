3b544de41ac360d94d1734f40bd4589f434d150b
forkdogforkfruit <119700133+forkdogforkfruit@users.noreply.github.com>
2023-09-08 16:08:22 +0200

    modified pictures

--- routes/pictures.js
+++ routes/pictures.js
@@ -10,11 +10,11 @@ const s3 = new AWS.S3();
 router.get("/", async function (req, res, next) {
   var params = {
     Bucket: process.env.CYCLIC_BUCKET_NAME,
-    Delimiter: "/",
-    Prefix: "public/",
+    Delimiter: '/',
+    Prefix: 'public/'
   };
   var allObjects = await s3.listObjects(params).promise();
-  var keys = allObjects?.Contents.map((x) => x.Key);
+  var keys = allObjects?.Contents.map( x=> x.Key)
 
   const pictures = await Promise.all(
     keys.map(async (key) => {
